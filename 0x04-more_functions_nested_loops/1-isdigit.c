b192c74029379313946b0f9da3845ac8e2cba4c9
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2023-06-22 18:15:02 +0000

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for characters that are digits
- * @c: character 
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to be checked
  *
  * Return: 0 or 1
  */
-
 
 int _isdigit(int c)
 {
